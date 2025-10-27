eb402938ac64fa64915eca3011313844ed7322c1
Sanni <117445321+fsannicolo@users.noreply.github.com>
2025-10-27 10:23:33 +0000

    feat: aggiunti componenti header e footer

--- corso-angular/src/app/app.ts
+++ corso-angular/src/app/app.ts
@@ -1,10 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Bindings } from './bindings/bindings';
+import { NavBar } from './nav-bar/nav-bar';
+import { Header } from './header/header';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Bindings],
+  imports: [RouterOutlet, Bindings, NavBar, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
