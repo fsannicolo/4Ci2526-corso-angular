e0826ab8618492642853aace313f840827ec7022
Sanni <117445321+fsannicolo@users.noreply.github.com>
2025-10-27 11:15:28 +0000

    feat: aggiunte immagini

--- corso-angular/src/app/bindings/bindings.ts
+++ corso-angular/src/app/bindings/bindings.ts
@@ -1,4 +1,5 @@
 import { Component, signal, WritableSignal } from '@angular/core';
+import { withIncrementalHydration } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-bindings',
@@ -7,10 +8,25 @@ import { Component, signal, WritableSignal } from '@angular/core';
   styleUrl: './bindings.css'
 })
 export class Bindings {
-  // crea un signal se si aggiorna alla modifica
+  // crea un signal che si aggiorna alla modifica
   studente: WritableSignal<string> = signal("Federico");
+  contatore: WritableSignal<number> = signal(0);
+
+  immagini: string[] = ['/rubik.png', '/pyraminx_scale.png', '/mirror_scale.png'];
+  indiceImg: WritableSignal<number> = signal(0);
 
   cambiaNome(): void {
     this.studente.set("Luca Boschi");
+  }
+
+  incrementa(): void {
+    // il metodo update usa:
+    // - valore attuale del signal
+    // - funzione di callback
+    this.contatore.update(valorePrecedente => valorePrecedente + 1);
+  }
+
+  decrementa(): void {
+    this.contatore.update(valorePrecedente => valorePrecedente - 1);
   }
 }
